16f39589013c6b6a79310ededc7c88e05ed31776
Rafael Ito <rafael.ito@ifood.com.br>
2020-11-24 01:57:53 -0300

    Initial commit

---
+++ src/main/java/com/learn/spring/JavaSpringLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringLearnApplication.class, args);
+	}
+
+}
